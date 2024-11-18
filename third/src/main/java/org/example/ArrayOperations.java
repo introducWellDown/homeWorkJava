d0e386226ce86245237d89ee83228190b6691cfc
Igor <maksimovPostup@yandex.ru>
2024-11-18 17:14:36 +0300

    проблема с БД

--- third/src/main/java/org/example/ArrayOperations.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public interface ArrayOperations<T> {
-    T[] reverse(T[] array);
-    T[] sortAscending(T[] array);
-    T[] sortDescending(T[] array);
-    T[] concatenate(T[] firstArray, T[] secondArray);
-    T[] append(T[] array, T value);
-}
